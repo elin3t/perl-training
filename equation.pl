dcf49bbc78df8c085dc775c18e6edae339d4e5d8
Eliecer Daza <eliecer.daza@globant.com>
2015-10-22 10:30:22 -0300

    Quadratic equation exercise v 0.1

---
+++ equation.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+use Math::Complex;
+
+my $a=0;
+my $b=0;
+my $c=0;
+
+($a, $b, $c)=@ARGV;
+sub main(){
+  if($b**2 < 4*$a*$c){
+    print STDOUT "Solution is not a real number \n";
+    return $b**2 < 4*$a*$c
+  }
+  my $x1 = ($b-sqrt($b**2 - 4*$a*$c))/(2*$a);
+  my $x2 = ($b+sqrt($b**2 - 4*$a*$c))/(2*$a);
+  print STDOUT "Solution 1: $x1\n";
+  print STDOUT "Solution 2: $x2\n";
+
+  return 0;
+}
+
+main();
+
+__END__
