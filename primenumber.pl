1b1059259f64687c305719a025743257344f6570
Eliecer Daza <eliecer.daza@globant.com>
2015-10-22 16:05:22 -0300

    prime numbers improvemets when 999999 near to 7segs

--- primenumber.pl
+++ primenumber.pl
@@ -10,25 +10,29 @@ sub is_valid_integer {
 
 sub is_prime_number{
   my $number = $_[0];
-  for my $divs (2..(ceil($number/2))){
-    if($number % $divs == 0){
+  if ($number % 2 == 0) {
+    return 0;
+  }
+
+  for (my $var = 3; $var < sqrt $number; $var+=2) {
+    if($number % $var == 0){
       return 0;
     }
   }
   return 1;
 }
-
+my @return;
 my $limit = 2;
 print STDOUT "welcome to Prime number list, \nplease enter a number between 1 and 1000000:\n";
 $limit = <STDIN>;
 chomp($limit);
 if(is_valid_integer($limit) && $limit >= 1 && $limit <= 1000000){
-  for my $number (1..$limit){
+  for(my $number = 1; $number<=$limit; $number+=2){
     if(is_prime_number($number)){
-      print STDOUT "$number ";
+      push @return, $number;
     }
   }
-  print STDOUT "\n";
+  print STDOUT join(' ', @return);
 }else{
   print STDERR "Please enter a valid number!\n"
 }
