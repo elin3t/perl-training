0368136798f78c8cca770047a7d1b33e79d9ee8d
Eliecer Daza <eliecer.daza@globant.com>
2015-10-29 13:30:17 -0300

    better code exercise 3 replace all

--- replace_indexes.pl
+++ replace_indexes.pl
@@ -4,28 +4,32 @@ use warnings;
 use List::MoreUtils qw(firstidx);
 use Data::Dumper;
 
-my %originals;
-my $sentence;
 
-print STDOUT "Please enter 1 line of text\n";
-$sentence = <>;
-chomp($sentence);
+sub main{
+    my %originals;
+    my $sentence;
 
-print STDOUT "Please, enter space separated strings to match and replace (one pair per line).\n";
-while(<>){
-  if($_ eq "\n"){
-      last;
-  }
-  my @tmp = split(/\s/,$_);
-  if(scalar @tmp == 2){
-    $originals{$tmp[0]} = $tmp[1];
-  }else{
-    print STDERR "Error: please enter 2 values for line, original text and replace text, space separated\n";
-  }
+    print STDOUT "Please enter 1 line of text\n";
+    $sentence = <>;
+    chomp($sentence);
+
+    print STDOUT "Please, enter space separated strings to match and replace (one pair per line).\n";
+    while(<STDIN>){
+      if($_ eq "\n"){
+          last;
+      }
+      my @tmp = split(/\s/,$_);
+      if(scalar @tmp == 2){
+        $originals{$tmp[0]} = $tmp[1];
+      }else{
+        print STDERR "Error: please enter 2 values for line, original text and replace text, space separated\n";
+      }
+    }
+    my $originals = join('|', keys %originals);
+    $sentence =~ s/($originals)/$originals{$1}/g;
+
+    say STDOUT "$sentence";
 }
-my $originals = join('|', keys %originals);
-$sentence =~ s/($originals)/$originals{$1}/g;
-
-say STDOUT "$sentence";
+main();
 
  __END__
